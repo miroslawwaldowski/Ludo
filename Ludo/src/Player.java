fa2719f9f70e27e28a13afd0694f7095d5ba8cec
Mirosław Waldowski <mwa@amu.edu.pl>
2019-02-07 00:39:01 +0100

    StartWindow ActionListener

--- Ludo/src/Player.java
+++ Ludo/src/Player.java
@@ -9,10 +9,12 @@ public class Player {
 	private int number;
 	private Color color;
 	private boolean active;
-
 	
+	private int typPlayerSet; //0 gracz, 1,2 komputer, 3 brak
 	private Choice typPlayer;
+	
 	private JLabel numberPlayer;
+	private String name;
 	private JTextField namePlayer;
 	
 
@@ -22,33 +24,29 @@ public class Player {
   
 	public Color getColor () {return color;}
 	public int getNumber () {return number;}
+	public String getName () {return name;}
 	public Choice getTypPlayer() {return typPlayer;}
 	public JLabel getNumberPlayer() {return numberPlayer;}
 	public JTextField getNamePlayer() {return namePlayer;}
 	public Pawn getPawn (int i) {return pawn[i];}
 	public boolean getActive () {return active;}
-
+	public int getTypPlayerSet () {return typPlayerSet;}
 	
-	public void setActive (boolean b) {
-		this.active = b;
-	}
-	
+	public void setActive (boolean b) {this.active = b;}
+	public void setName (String s) {this.name = s;}
+	public void setTypPlayerSet (int typPlayerSet) {this.typPlayerSet = typPlayerSet;}
+	public void setTypPlayer(Choice typPlayer) {this.typPlayer = typPlayer;}
 	public void setColor (Color color) {
 		this.color = color;
 		this.numberPlayer.setBackground(this.color);
 		this.numberPlayer.setOpaque(true);
 		}
-	
 	public void setNumber (int number) {
 		this.number = number;
 		this.numberPlayer.setText(Integer.toString(this.number+1));
 		}
-	
-	public void setTypPlayer(Choice typPlayer) {this.typPlayer = typPlayer;}
 
-	
-	
-	
+
 	public Player() {
 		
 		this.namePlayer=new JTextField();
